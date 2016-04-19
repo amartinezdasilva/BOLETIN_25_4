fe23b1088cfc50877ff28580a4f6e0da0b3f3e0f
Aaron <Aaron@DESKTOP-BCLEH27>
2016-04-19 09:38:03 +0200

    final

---
+++ src/boletin_25/pkg4/Boletin_254.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_25.pkg4;
+
+/**
+ *
+ * @author Aaron
+ */
+public class Boletin_254 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Frame bila = new Frame();
+       bila.setVisible(true);
+    }
+    
+}
